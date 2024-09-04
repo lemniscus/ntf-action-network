c88071472d6395db107d912cc200f1063b3c8b3d
Noah Miller <nm@lemnisc.us>
2024-09-03 21:48:22 -0400

    remove obsolete code

--- Civi/Osdi/ActionNetwork/SingleSyncer/PersonN2F.php
+++ Civi/Osdi/ActionNetwork/SingleSyncer/PersonN2F.php
@@ -10,46 +10,4 @@ use Civi\Osdi\Util;
 
 class PersonN2F extends PersonBasic implements SingleSyncerInterface {
 
-  protected function getRemoteObjectClass(): string {
-    return \Civi\Osdi\ActionNetwork\Object\Person::class;
-  }
-
-  /*
-   * unused function. also does this logic even make sense? seems like it should
-   * be the opposite
-   *
-   *  protected function newRemoteShouldBeCreatedForLocal(LocalRemotePair $pair): bool {
-   *    $localPerson = $pair->getLocalObject();
-   *    $localPerson->loadOnce();
-   *    $doNotEmail = $localPerson->doNotEmail->get();
-   *    $emailOnHold = $localPerson->emailOnHold->get();
-   *    $emailIsDummy = 'noemail@' === substr($localPerson->emailEmail->get(), 0, 8);
-   *    $doNotSms = $localPerson->doNotSms->get();
-   *
-   *    if ($doNotEmail || $emailIsDummy || $emailOnHold) {
-   *      if ($doNotSms || empty($localPerson->smsPhonePhone->get())) {
-   *        return TRUE;
-   *      }
-   *    }
-   *
-   *    return FALSE;
-   *  }
-   */
-
-  protected function typeCheckRemotePerson(RemoteObjectInterface $object): \Civi\Osdi\ActionNetwork\Object\Person {
-    Util::assertClass($object, \Civi\Osdi\ActionNetwork\Object\Person::class);
-    /** @var \Civi\Osdi\ActionNetwork\Object\Person $object */
-    return $object;
-  }
-
-  protected function getLocalObjectClass(): string {
-    return \Civi\Osdi\LocalObject\PersonN2F::class;
-  }
-
-  private function typeCheckLocalPerson(LocalObjectInterface $object): \Civi\Osdi\LocalObject\PersonN2F {
-    Util::assertClass($object, \Civi\Osdi\LocalObject\PersonN2F::class);
-    /** @var \Civi\Osdi\LocalObject\PersonN2F $object */
-    return $object;
-  }
-
 }
